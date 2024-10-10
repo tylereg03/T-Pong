5687e3950b9bd4ae0935845077f72d26f7310602
Tyler Guthrie <tylerevanguthrie@gmail.com>
2024-10-10 14:50:50 -0700

    State Machine Addition
    Major overhaul to the logic of the game. Instead of the root node being the game, it is now a state machine which runs on either a menu or playing state. The menu state machine will contain all current and future menus. Currently, the gameplay state machine is slightly underwhelming in its functionality since a lot of gameplay logic is found in the ball scene, which is almost certainly subject to change. This will hopefully allow the game to be much more scalable and make tasks such as adding game over end screens and new menus much easier.

---
+++ scripts/menu.gd
@@ -0,0 +1,105 @@
+extends CanvasLayer
+
+class_name Menu
+
+# MENU STATE MACHINE
+enum MenuState {
+	MAIN_MENU,
+	PLAYER_SELECT,
+	SKIN_SELECT,
+	GAME_START
+}
+
+
+# signaled when the player enters game_start
+signal game_start(behavior, p1_sprite, p2_sprite)
+
+
+# used to store the current state we are in, defaulting to main menu
+var curr_state
+
+
+# used to store information extracted from skin_selector
+var behavior
+var p1_sprite
+var p2_sprite
+
+# This function is responsible for setting new states
+func set_state(new_state):
+	curr_state = new_state
+	match curr_state:
+		MenuState.MAIN_MENU:
+			print("main menu active")
+			$NumPlayers.hide()
+			$MainMenu.show()
+		MenuState.PLAYER_SELECT:
+			print("player select active")
+			$MainMenu.hide()
+			$SkinSelector.hide()
+			$NumPlayers.show()
+		MenuState.SKIN_SELECT:
+			print("skin select active")
+			$NumPlayers.hide()
+			$SkinSelector.show()
+		MenuState.GAME_START:
+			print("game start active")
+			$SkinSelector.hide()
+			game_start.emit(behavior, p1_sprite, p2_sprite)
+
+# Play Button in Main Menu pressed
+func _on_play_pressed():
+	set_state(MenuState.PLAYER_SELECT)
+
+# Quit Button in Main Menu pressed
+func _on_quit_button_down():
+	get_tree().quit()
+
+
+# 1 Player option pressed in Player select
+func _on_p_1_button_down():
+	# hide 2P specifics and show 1P specifics
+	$SkinSelector/Difficulties.show()
+	$SkinSelector/DifficultiesTexts.show()
+	$SkinSelector/SkinSelect2.hide()
+	
+	# have easy automatically toggled to true
+	$SkinSelector._on_easy_toggled(true)
+	
+	set_state(MenuState.SKIN_SELECT)
+
+
+# 2 Player option pressed in Player select
+func _on_p_2_button_down():
+	# hide 1P specifics and show 2P specifics
+	$SkinSelector/Difficulties.hide()
+	$SkinSelector/DifficultiesTexts.hide()
+	$SkinSelector/SkinSelect2.show()
+	
+	# toggle easy to false if 2 players are selected
+	$SkinSelector._on_easy_toggled(false)
+	$SkinSelector.behavior = Paddle.Behavior.PLAYER
+	
+	set_state(MenuState.SKIN_SELECT)
+
+
+# Skins chosen inside of skin selector, this will call for a new game to start
+func _on_skin_selector_start():
+	behavior = $SkinSelector.behavior
+	p1_sprite = $SkinSelector.p1_sprite
+	p2_sprite = $SkinSelector.p2_sprite
+	
+	set_state(MenuState.GAME_START)
+
+# This keeps track of which hud element is currently visible, in order to
+# have "esc" either backtrack, or quit the game
+func _input(event):
+	if event.is_action_pressed("esc"):
+		match curr_state:
+			MenuState.MAIN_MENU:
+				get_tree().quit()
+			MenuState.PLAYER_SELECT:
+				set_state(MenuState.MAIN_MENU)
+			MenuState.SKIN_SELECT:
+				set_state(MenuState.PLAYER_SELECT)
+			MenuState.GAME_START:
+				pass
---
+++ scripts/state_machine.gd
@@ -0,0 +1,41 @@
+extends Node
+
+enum MasterState {
+	MENU,
+	PLAYING
+}
+
+var curr_state
+
+
+@onready var menu_state_machine = $Menu
+@onready var gameplay_state_machine = $Game
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	menu_state_machine.set_state(Menu.MenuState.MAIN_MENU)
+	set_state(MasterState.MENU)
+
+# This function is responsible for setting new states
+func set_state(new_state):
+	curr_state = new_state
+	match curr_state:
+		MasterState.MENU:
+			print("IN MENU")
+			menu_state_machine.show()
+			gameplay_state_machine.hide()
+		MasterState.PLAYING:
+			print("IN PLAYING")
+			menu_state_machine.hide()
+			gameplay_state_machine.show()
+
+
+# Called when menu state is GAME_START
+func _on_hud_game_start(behavior, p1_sprite, p2_sprite):
+	gameplay_state_machine.new_game(behavior, p1_sprite, p2_sprite)
+	set_state(MasterState.PLAYING)
+
+
+func _on_game_game_end():
+	menu_state_machine.set_state(Menu.MenuState.MAIN_MENU)
+	set_state(MasterState.MENU)
